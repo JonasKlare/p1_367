21a029cb4b97a1a5ad3fde9ab1a75db61f2e0d1c
jdubois52 <jdubois3@wisc.edu>
2017-02-08 17:58:40 -0600

    Add files via upload

--- src/GradeFileFormatException.java
+++ src/GradeFileFormatException.java
@@ -1,4 +1,10 @@
 
-public class GradeFileFormatException {
-
+public class GradeFileFormatException extends Exception {
+	
+	public GradeFileFormatException(){
+		super();
+	}
+	public GradeFileFormatException(String msg){
+		super(msg);
+	}
 }
